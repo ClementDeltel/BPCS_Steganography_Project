a4ad6d5c344933d128302fc8e074e26e16d550be
David Galan Berasaluce <dgalanberasaluce@hawk.iit.edu>
2018-11-02 18:09:04 -0500

    Set default value for numbers of bitplane to 16

--- segmentation.m
+++ segmentation.m
@@ -1,20 +1,21 @@
-%Function that segment each bit-plane of the image into informative and
-%noise-like regions using a thresholdvalue
-%Input: Matrix bitplane (NxNx8)
+%Function that group each bit-plane of the image into informative or
+%noise-like regions. The bitplane presents a noise-like pattern when its 
+%complexity is higher than the threshold value (alpha)
+%Input: Matrix bitplane (bp x N x M)
 %Output: indexes of the noise & informative bitplanes
 
-%Threshold: replace each pixel in an image with a black pixel if the image
-%intensity is less than a constant (alpha)
-function [noise,informative] = segmentation(BP,alpha)  
+function [noise,informative] = segmentation(BP,alpha,num_bp)  
     if nargin < 2
+        num_bp = 16;
         alpha=0.3;
+    elseif nargin < 3
+        num_bp = 16;
     end
+    
     noise = [];
     informative = [];
-    num_bp = 8;
     
     for i=1:num_bp
-        %complexity = [complexity get_complexity(BP(:,:,i))];
         if (get_complexity(BP(i,:,:)) >= alpha)
             noise = [noise i];
         else
